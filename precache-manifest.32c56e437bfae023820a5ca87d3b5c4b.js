81b6130b09a0bbeafafc5f06bbf75ff04c4220f0
kanzariamine <kanzari.amin@gmail.com>
2018-11-10 11:41:34 +0100

    Updates

---
+++ precache-manifest.32c56e437bfae023820a5ca87d3b5c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d721b1019249664737b3",
+    "url": "/React-Countdown/static/js/runtime~main.d721b101.js"
+  },
+  {
+    "revision": "e67811b33685f765cb68",
+    "url": "/React-Countdown/static/js/main.e67811b3.chunk.js"
+  },
+  {
+    "revision": "68801e0b4057ca8338c1",
+    "url": "/React-Countdown/static/js/1.68801e0b.chunk.js"
+  },
+  {
+    "revision": "e67811b33685f765cb68",
+    "url": "/React-Countdown/static/css/main.7093077b.chunk.css"
+  },
+  {
+    "revision": "68801e0b4057ca8338c1",
+    "url": "/React-Countdown/static/css/1.291d1279.chunk.css"
+  },
+  {
+    "revision": "90a2f8d96e8c9cbc8c56bf54f1cdbb39",
+    "url": "/React-Countdown/index.html"
+  }
+];
